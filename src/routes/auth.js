43289a1db3cd3cce55ea5f172b2d2d41dfd16bfb
cakmakg <cakmak4834@gmail.com>
2025-04-04 10:20:49 +0200

    auth routes update

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,3 +4,15 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+
+const { login, refresh, logout } = require('../controllers/auth');
+
+// URL: /auth
+
+router.post('/login', login);
+router.post('/refresh', refresh);
+router.get('/logout', logout);
+
+
+/* ------------------------------------------------------- */
+module.exports = router;
